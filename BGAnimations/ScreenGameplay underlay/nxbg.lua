7c2a4a9966c93172825338b6a16106d7f11ad96e
pupsicola <56452648+pupsicola@users.noreply.github.com>
2023-05-04 01:43:17 -0500

    Update Judgment

--- BGAnimations/ScreenGameplay underlay/nxbg.lua
+++ BGAnimations/ScreenGameplay underlay/nxbg.lua
@@ -12,32 +12,6 @@ t[#t+1] = Def.ActorFrame {
 			InitCommand=cmd();
 			OnCommand=cmd(Center;zoom,2.665;diffusealpha,0.4;)
 		};
-		
-				Def.ActorFrame{
-		Name="Hot";
-		Def.ActorFrame{
-			Name="Heat";
-			HealthStateChangedMessageCommand=function(self, param)
-				if param.PlayerNumber == PLAYER_1 then
-					if param.HealthState == "HealthState_Hot" then
-						self:RunCommandsOnChildren(cmd(playcommand,"Show"))
-					else
-						self:RunCommandsOnChildren(cmd(playcommand,"Hide"))
-					end
-				end
-			end;
-			LoadActor("particlenormal") .. {
-			InitCommand=cmd(diffusealpha,0;);
-			ShowCommand=cmd(diffusealpha,0.25;);
-			HideCommand=cmd(diffusealpha,0;);
-		};
-			LoadActor("_LoadingBar")..{
-				InitCommand=cmd(blend,'BlendMode_Add';diffusealpha,0;texcoordvelocity,0.2,0;Center;zoomx,3;zoomy,500;thump;effectclock,'beat';effectmagnitude,2,1,1;effectoffset,0;);
-				ShowCommand=cmd(diffusealpha,0.05);
-				HideCommand=cmd(diffusealpha,0);
-			};
-		}
-		},
 
 		Def.ActorFrame{
 		Name="Danger";
@@ -82,7 +56,32 @@ t[#t+1] = Def.ActorFrame {
 		LoadActor("spiral") .. {
 		InitCommand=cmd(x,Center1Player() and SCREEN_CENTER_X or SCREEN_CENTER_X+200;y,SCREEN_CENTER_Y;diffusealpha,0.1;zoom,1.2;rotationx,-45;blend,'BlendMode_Add';effectperiod,10;spin;effectmagnitude,0,0,5)
 		};
-
+		
+		Def.ActorFrame{
+		Name="Hot";
+		Def.ActorFrame{
+			Name="Heat";
+			HealthStateChangedMessageCommand=function(self, param)
+				if param.PlayerNumber == PLAYER_1 then
+					if param.HealthState == "HealthState_Hot" then
+						self:RunCommandsOnChildren(cmd(playcommand,"Show"))
+					else
+						self:RunCommandsOnChildren(cmd(playcommand,"Hide"))
+					end
+				end
+			end;
+			LoadActor("particlenormal") .. {
+			InitCommand=cmd(diffusealpha,0;);
+			ShowCommand=cmd(diffusealpha,0.25;);
+			HideCommand=cmd(diffusealpha,0;);
+		};
+			LoadActor("_LoadingBar")..{
+				InitCommand=cmd(blend,'BlendMode_Add';diffusealpha,0;texcoordvelocity,0.2,0;Center;zoomx,3;zoomy,500;thump;effectclock,'beat';effectmagnitude,2,1,1;effectoffset,0;);
+				ShowCommand=cmd(diffusealpha,0.05);
+				HideCommand=cmd(diffusealpha,0);
+			};
+		}
+		},
 		
 		Def.ActorFrame{
 			HealthStateChangedMessageCommand=function(self, param)
