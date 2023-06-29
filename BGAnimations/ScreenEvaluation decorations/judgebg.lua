8509caf51103e10d9ffb7c14587b194d1bc24c62
pupsicola <56452648+pupsicola@users.noreply.github.com>
2023-06-29 18:17:35 -0500

    slowfast update

--- BGAnimations/ScreenEvaluation decorations/judgebg.lua
+++ BGAnimations/ScreenEvaluation decorations/judgebg.lua
@@ -3,8 +3,8 @@ local t = Def.ActorFrame {};
 
 t[#t+1] = LoadActor("judgment");
 
-if ReadPrefFromFile("UserPrefGameplayShowFastSlow") ~= nil then
-	if GetUserPrefB("UserPrefGameplayShowFastSlow") then
+if ThemePrefs.Get("SlowFast") == "Off" then
+	if ThemePrefs.Get("SlowFast") == "On" then
 		t[#t+1] = LoadActor("timingbg");
 	end
 end
