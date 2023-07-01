3435ddfae036dfab821ab14b323442a120cc3e57
pupsicola <56452648+pupsicola@users.noreply.github.com>
2023-06-30 20:08:28 -0500

    fixed user prefs not saving

--- BGAnimations/ScreenTitleMenu underlay.lua
+++ BGAnimations/ScreenTitleMenu underlay.lua
@@ -1,5 +1,15 @@
-local t = Def.ActorFrame{};
+InitUserPrefs();
 
+local t = Def.ActorFrame {
+	OnCommand=function(self)
+		if not FILEMAN:DoesFileExist("Save/ThemePrefs.ini") then
+			Trace("ThemePrefs doesn't exist; creating file")
+			ThemePrefs.ForceSave()
+		end
+
+		ThemePrefs.Save()
+	end;
+};
 t[#t+1] = Def.ActorFrame {
 InitCommand=cmd();
 		LoadActor( "ScreenLogo decorations/3dlogo" )..{
