b201c0799183c7e660b5b9245ff5bf7b71302103
pupsicola <56452648+pupsicola@users.noreply.github.com>
2023-09-06 18:16:01 -0500

    MusicWheel fix

--- Graphics/MusicWheelItem SectionCollapsed NormalPart/default.lua
+++ Graphics/MusicWheelItem SectionCollapsed NormalPart/default.lua
@@ -7,50 +7,27 @@ local group;
 
 local t = Def.ActorFrame {
 
-
-Def.Sprite {
-		Name="GroupBG";
-		InitCommand=cmd(x,120;scaletoclipped,538,538;croptop,0.435;cropbottom,0.435;fadeleft,1;diffusealpha,0.35);
-		SetMessageCommand=function(self,params)
-			-- group = params.Text;
-			-- If the cached version is available, load that.
-			if NXCachedGroupAreas[params.Text] then
-				self:LoadFromCached("banner", NXCachedGroupAreas[params.Text] )
-				return
-			end
-			-- local so = GAMESTATE:GetSortOrder();
-			-- if group then
-			-- 	LoadFromSelectionBG(self, group, so)
-			-- else
-			-- 	-- call fallback
-			-- 	self:Load( THEME:GetPathG("","_No banner") );
-			-- end;
-		end;
-	};
-
-
 	LoadActor("MusicWheelItem SectionCollapsed NormalPart") .. {
 
 };
 --banner
 
-Def.Sprite {
+Def.Banner {
 		Name="SongBanner";
 		InitCommand=cmd(scaletoclipped,60,60;x,-235;);
 		SetMessageCommand=function(self,params)
-			-- group = params.Text;
-			-- If the cached version is available, load that.
-			if NXCachedGroupAreas[params.Text] then
-				self:LoadFromCached("banner", NXCachedGroupAreas[params.Text] )
-				return
-			end
-			-- local so = GAMESTATE:GetSortOrder();
-			-- if group then
-			-- 	LoadFromSelectionBG(self, group, so)
-			-- else
-			-- 	-- call fallback
-			-- 	self:Load( THEME:GetPathG("","_No banner") );
-			-- end;
+					group = params.Text;
+		local so = GAMESTATE:GetSortOrder();
+			if group then
+				
+					self:LoadFromSongGroup(group);
+				
+			else
+				
+				-- call fallback
+				self:Load( THEME:GetPathG("","_No banner") );
+			end;
+
 		end;
 	};
 
