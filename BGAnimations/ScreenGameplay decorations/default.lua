c9b2be43c6decf7dd5ce547ef4b36c1d131b1d12
pupsicola <56452648+pupsicola@users.noreply.github.com>
2023-09-19 23:39:06 -0500

    OutFox Scoring fix

--- BGAnimations/ScreenGameplay decorations/default.lua
+++ BGAnimations/ScreenGameplay decorations/default.lua
@@ -1,51 +1,19 @@
 local t = LoadFallbackB()
 
 
-local customscore=GetCustomScoreMode();
- local cscore="SuperNOVA2";
- if not GAMESTATE:IsCourseMode() then
- local stepcnt={0,0}
- t[#t+1] = Def.Actor{
-     JudgmentMessageCommand = function(self, params)
-		if STATSMAN:GetCurStageStats():GetPlayerStageStats(params.Player):GetFailed() == false then
-         if params.TapNoteScore and
-          params.TapNoteScore ~= 'TapNoteScore_AvoidMine' and
-          params.TapNoteScore ~= 'TapNoteScore_HitMine' and
-          params.TapNoteScore ~= 'TapNoteScore_CheckpointMiss' and
-          params.TapNoteScore ~= 'TapNoteScore_CheckpointHit' and
-          params.TapNoteScore ~= 'TapNoteScore_None'
-         then
-             if customscore=="old" then
-                 Scoring[scoreType](params, 
-                     STATSMAN:GetCurStageStats():GetPlayerStageStats(params.Player))
-             elseif customscore=="5b2" then
-                 local pn=((params.Player==PLAYER_1) and 1 or 2);
-                 stepcnt[pn]=stepcnt[pn]+1;
-                 CustomScore_SM5b2(params,cscore,GAMESTATE:GetCurrentSteps(params.Player),stepcnt[pn]);
-             elseif customscore=="5b1" then
-                 local pn=((params.Player==PLAYER_1) and 1 or 2);
-                 stepcnt[pn]=stepcnt[pn]+1;
-                 CustomScore_SM5b1(params,cscore,GAMESTATE:GetCurrentSteps(params.Player),stepcnt[pn]);
-             else
-                 local pn=((params.Player==PLAYER_1) and 1 or 2);
-                 stepcnt[pn]=stepcnt[pn]+1;
-                 CustomScore_SM5a2(params,cscore,GAMESTATE:GetCurrentSteps(params.Player),stepcnt[pn]);
-             end;
-         end
-		 end
-     end;
-     InitCommand=function(self)
-         if customscore=="non" then
-             CustomScore_SM5a2_Init();
-         end;
-     end;
-     OffCommand=function(self)
-         if customscore=="non" then
-             CustomScore_SM5a2_Out();
-         end;
-     end;
- };
- end;
+t[#t+1] = StatsEngine()
+
+t[#t+1] = Def.Actor{
+    AfterStatsEngineMessageCommand = function(self, params)
+        local pn = params.Player
+        local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn)
+
+        local aScore = params.Data.AScoring
+        pss:SetScore(aScore.Score)
+        pss:SetCurMaxScore(aScore.MaxScore)
+
+	end;
+};
  
 
 t[#t+1] = LoadActor("OptionsHack.lua")..{
@@ -134,6 +102,8 @@ t[#t+1] = Def.ActorFrame{
 	InitCommand=cmd(maxwidth,240;shadowlengthy,2;zoom,0.75;x,Center1Player() and SCREEN_CENTER_X-140 or SCREEN_CENTER_X-508;y,SCREEN_BOTTOM-35;draworder,5;addy,100;);
 	OnCommand=cmd(decelerate,2;addy,-100;);
 	};
+
+t[#t+1] = LoadActor("ScoreFrame")..{ InitCommand=function(s) s:draworder(99) end, };
 
 
 return t
